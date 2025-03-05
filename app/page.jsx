358b007c70acecbb5dbab5ee25111a4d0ccf08e6
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-03-05 05:48:19 +0100

    update 1 game

--- app/page.jsx
+++ app/page.jsx
@@ -28,13 +28,13 @@ const App = () => {
 
   // Game function
   const generateGame = () => {
-    const randomColors = Array.from({ length: 25 }, () => generateRandomColor());
+    const randomColors = Array.from({ length: 9 }, () => generateRandomColor());
     setColors(randomColors);
 
     const target = randomColors[Math.floor(Math.random() * 1)];
     setTargetColor(target);
 
-    const options = [target, ...Array.from({ length: 8 }, () => generateRandomColor())].sort(
+    const options = [target, ...Array.from({ length: 4 }, () => generateRandomColor())].sort(
       () => Math.random() - 0.5
     );
     setGuessOption(options);
@@ -60,7 +60,7 @@ const App = () => {
       setMessage('✅ Correct!');
       setScore((prev) => prev + 1);
     } else {
-      setMessage('❌ Oops! Try Again.');
+      setMessage('❌ Wrong! Try Again.');
     }
 
     setTries((prevTries) => {
@@ -70,9 +70,9 @@ const App = () => {
         
         const finalScore = isCorrect ? score + 1 : score;
         if (finalScore === 10) {
-          setMessage('🎉 Congratulations, YOU WON! 🍻🍺 ON ON');
+          setMessage('🎉 Congratulations, YOU WON!');
         } else {
-          setMessage(`OTILO🙋‍♂️🙋‍♀️ Game Over! Total score: ${finalScore}`);
+          setMessage(`Game Over! Total score: ${finalScore}`);
         }
       } else {
         setTimeout(generateGame, 1000);
@@ -82,7 +82,7 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center pt-6 px-[1rem] space-y-2 md:space-y-3 mb-16">
+    <div className="flex flex-col items-center justify-center pt-6 px-[3rem] space-y-2 md:space-y-3 pb-10">
       <div className="flex items-center gap-3">
         <p className="text-4xl text-red-400/80"><IoColorPaletteSharp /></p>
         <p className="font-bold text-base md:text-2xl lg:text-4xl bg-gradient-to-r from-rose-400 via-violet-600 to-blue-600 tracking-[5px] md:tracking-[10px] lg:tracking-[20px] bg-clip-text text-transparent">
@@ -93,22 +93,22 @@ const App = () => {
       <h1 data-testid="gameInstructions" className="text-sm md:text-lg">Guess the correct color from the displayed random colors below.</h1>
 
       {/* scores, tries & new game */}
-      <div className=" md:ml-auto text-sm md:text-base flex flex-col space-y-5 pt-3  md:px-[5rem]">
+      <div className=" md:ml-auto text-sm md:text-base flex flex-col gap-1  md:px-[5rem]">
         <h3 data-testid="score" className="">Score: {score}</h3>
         <h3>Tries: {tries}/10</h3>
         <button data-testid="newGameButton" className="submit w-full md:mt-1 flex items-center gap-2 justify-center" onClick={resetGame}>New Game <h1 className="text-xl"><RiResetLeftLine /></h1></button>
       </div>
 
       {/* target color */}
-      <div className="">
-        <p className="pb-1 md:text-xl text-center mt-5">Target color</p>
-        <div data-testid="colorBox" className=" p-3 md:p-6 bg-gray-800 rounded-lg shadow-lg grid grid-cols-5 gap-5">
-          {/* <p className="pb-1 md:text-xl ">Target color</p> */}
+      <div className="flex flex-col">
+      <p className="pb-1 md:text-xl text-center">Target color</p>
+        <div data-testid="colorBox" className="grid grid-cols-3 gap-3 p-3 md:p-6 bg-gray-800 rounded-lg shadow-lg">
+          
           {colors.map((color, index) => (
             <div
               key={index}
               style={{ backgroundColor: color }}
-              className="w-12 h-12 md:h-12 md:w-12 rounded-lg"
+              className="w-16 h-16 md:h-16 md:w-16 rounded-lg"
             ></div>
           ))}
         </div>
@@ -131,7 +131,6 @@ const App = () => {
       </div>
 
       {message && <p data-testid="gameStatus" className="md:text-lg font-bold blink-1 text-sm">{message}</p>}
-
 
 
       {/* Footer */}
